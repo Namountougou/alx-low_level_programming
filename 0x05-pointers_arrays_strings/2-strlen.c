69b88d1f1b523d3108c9a3e50a13fd2fcc7e86fb
Namountougou <naboure@gmail.com>
2022-10-24 10:35:40 -0700

    strlen 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - print a length of a string
+ * @s: var char
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != "\0")
+	{
+		len++;
+		S++;
+	}
+	return (len);
+}
+
