0cea72397570311fd410750f462620f6d699d320
Namountougou <naboure@gmail.com>
2022-10-24 12:15:35 -0700

    rev 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,14 @@
 /**
  * print_rev - is a function that reverse a string.
  * @s: var
- */
+*/
 void print_rev(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
 		i++;
-	for (i = i-1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
