b499e11b5d7a15ce3e7805f6da4d6eac6aa79144
Namountougou <naboure@gmail.com>
2022-10-24 11:38:06 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _puts - it is a function that prints a string
+ * @str: var char
+ * Return: always 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		printf(*str++);
+	}
+	printf("\n");
+}
