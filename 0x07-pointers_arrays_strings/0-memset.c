1e59fbc58013e6ab6f4ea04ba4c667613467ecd3
Namountougou <naboure@gmail.com>
2022-11-02 09:26:47 -0700

    yoo

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,9 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int size = n;
-
-	if (size > 0)
+	if (n > 0)
 	{
-		int i;
+		unsigned int i;
 
 		for (i = 0; i < size; i++)
 			s[i] = b;
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,15 +7,12 @@
  * @n: number of bytes
  * Return: pointer to dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
-	int size = n; /* only accept positive sizes */
-
-	if (size > 0)
+                                    
+	if (n > 0)
 	{
-		int i;
+		unsigned int i;
 
 		for (i = 0; i < size; i++)
 			dest[i] = src[i];
